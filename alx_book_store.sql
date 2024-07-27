11abcd9a6984bccc6f599d9c6804b677716881e3
Ecode2 <ecode5814@gmail.com>
2024-07-27 20:35:24 +0100

    Initial commit

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    Foreign Key (author_id) REFERENCES Authors (id),
+    price DOUBLE,
+    publiation_date DATE
+);
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE NOT NULL
+);
+
+CREATE Table Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    Foreign KEY (order_id) REFERENCES Orders (order_id),
+    Foreign KEY (book_id) REFERENCES Books (book_id),
+    quantity FLOAT
+);
