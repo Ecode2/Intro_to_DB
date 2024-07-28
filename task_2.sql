08aade41a419365a33dc63c055ee50f9b55a2cf5
Abubakar Alaya <ecode5814@gmail.com>
2024-07-28 16:41:15 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,2 +1,3 @@
 CREATE TABLE Authors, author_id , author_name
 CREATE TABLE Books, title, price, PRIMARY KEY
+CREATE TABLE Customers, customer_id , customer_name VARCHAR(215), email VARCHAR(215), address TEXT
