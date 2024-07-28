cfad359606641c745a03dfa768efe27f86d293fa
Abubakar Alaya <ecode5814@gmail.com>
2024-07-28 22:18:25 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,4 @@
-import mysql.connector as msc
-mydb = mac.connect()
+import mysql.connector
+mydb = mysql.connector.connect()
 mycur =mydb.cursor() 
 mycur.exectute("CREATE DATABASE IF NOT EXISTS alx_book_store")
