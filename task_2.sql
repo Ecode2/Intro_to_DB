3c0317af8832b7bcc34d3ddc3431b96c37bc74d6
Abubakar Alaya <ecode5814@gmail.com>
2024-07-28 16:44:49 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,5 @@
 CREATE TABLE Authors, author_id , author_name
 CREATE TABLE Books, title, price, PRIMARY KEY
 CREATE TABLE Customers, customer_id , customer_name VARCHAR(215), email VARCHAR(215), address TEXT
+CREATE TABLE Orders, order_id INT PRIMARY KEY, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), order_date DATE NOT NULL
+CREATE TABLE order_detail_id INT PRIMARY KEY, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id), quantity DOUBLE
