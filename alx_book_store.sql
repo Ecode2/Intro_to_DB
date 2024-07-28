6c18f8a8788c89f05b6fedb7e38d073f3e7cd914
Abubakar Alaya <ecode5814@gmail.com>
2024-07-28 16:29:30 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,5 +32,5 @@ CREATE Table Order_Details (
     order_detail_id INT PRIMARY KEY,
     Foreign KEY (order_id) REFERENCES Orders (order_id),
     Foreign KEY (book_id) REFERENCES Books (book_id),
-    quantity FLOAT
+    quantity DOUBLE
 );
